97c86e219a5cefd0c2d215ffc6ffa68e8f4a5f0c
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-04-10 16:57:41 +0800

    add in new data for bookings

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,15 +1,34 @@
+import { useState, useEffect } from "react";
 import bookings from "../../bookings.js";
 
 export default function Schedule() {
   const appointments = bookings.bookings.filter(
     (booking) => booking.practitioner === "Dr. Garcia"
   );
+  const [day, setDay] = useState(1);
+  const [dayName, setDayName] = useState("Monday");
+
+  useEffect(() => {
+    if (day === 1 || day === 6) {
+      setDayName("Monday");
+    }
+    if (day === 2 || day === 7) setDayName("Tuesday");
+    if (day === 3) {
+      setDayName("Wednesday");
+    }
+    if (day === 4) {
+      setDayName("Thursday");
+    }
+    if (day === 5) {
+      setDayName("Friday");
+    }
+  }, [day]);
 
   function Time(props) {
     let now = new Date();
     now.setDate(now.getDate() + 1);
-    now.setHours(props.time);
-    now.setMinutes(0);
+    now.setHours(props.hour);
+    now.setMinutes(props.minute);
     now.setSeconds(0);
     now.setMilliseconds(0);
 
@@ -24,31 +43,64 @@ export default function Schedule() {
   //   console.log(appointments.practitioner);
   return (
     <div>
-      <h2>{appointments[0].practitioner}</h2>
-      {appointments.map((appointment, idx) => {
-        return (
-          <>
-            <div
-              key={idx}
-              style={{ border: "1px solid black", padding: "1em" }}
-            >
-              <p>{appointment.patient}</p>
+      <h1>Greetings, {appointments[0].practitioner}</h1>
 
-              <p>{appointment.type}</p>
-              <div
-                style={{
-                  margin: "1em 0",
-                  textAlign: "center",
-                  fontWeight: "bold",
-                }}
-              >
-                <Time time={appointment.startTime} /> -
-                <Time time={appointment.endTime} />
+      <div className="appointments">
+        <div className="day-selector">
+          <button
+            disabled={day === 1}
+            onClick={() => setDay((prev) => prev - 1)}
+          >
+            prev
+          </button>
+          <button
+            disabled={day === 7}
+            onClick={() => setDay((prev) => prev + 1)}
+          >
+            next
+          </button>
+        </div>
+        <h1>{dayName}</h1>
+        {appointments
+          .filter((elm) => elm.day === day)
+          .map((appointment, idx) => {
+            return (
+              <div className="appointment-container">
+                <div className="time-index">
+                  <p>
+                    <Time
+                      hour={appointment.startTimeH}
+                      minute={appointment.startTimeM}
+                    />{" "}
+                  </p>
+                </div>
+                <div className="appointment" key={idx}>
+                  <div
+                    style={{
+                      margin: "1em 0",
+                      textAlign: "center",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    <Time
+                      hour={appointment.startTimeH}
+                      minute={appointment.startTimeM}
+                    />{" "}
+                    -
+                    <Time
+                      hour={appointment.endTimeH}
+                      minute={appointment.endTimeM}
+                    />
+                  </div>
+                  <p style={{ fontWeight: "bold" }}>{appointment.type}</p>
+                  <p>{appointment.patient}</p>
+
+                  <p>{appointment.duration} minutes</p>
+                </div>
               </div>
-            </div>
-          </>
-        );
-      })}
+            );
+          })}
+      </div>
     </div>
   );
 }
